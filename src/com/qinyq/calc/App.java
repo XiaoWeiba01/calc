e8d1874a1ae44f23e6029f76bcfa85b74b39349e
qinyq <qyqiong@qq.com>
2017-04-01 15:48:31 +0800

    设置了UI

--- src/com/qinyq/calc/App.java
+++ src/com/qinyq/calc/App.java
@@ -3,7 +3,8 @@ package com.qinyq.calc;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		CalcFrame frame=new CalcFrame();
 
 	}
 
--- src/com/qinyq/calc/CalcFrame.java
+++ src/com/qinyq/calc/CalcFrame.java
@@ -1,8 +1,78 @@
 package com.qinyq.calc;
 
-import javax.swing.JFrame;
+import java.awt.BorderLayout;
 
+
+import java.awt.Font;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+
+/**
+ * 计算器窗口
+ * @author 琼宝宝
+ *
+ */
 public class CalcFrame extends JFrame{
+	
+	/**
+	 * 显示计算结果
+	 */
+	private JLabel labelResult;
+	/**
+	 * 按钮上的标签
+	 */
+	private String [] titles={"","","","C","7","8","9","/","4","5","6","-","1","2","3",".","*","0","+","="};
+	/**
+	 * 按钮
+	 */
+	private JButton[]buttons=new JButton[titles.length];
+	
+	
+	public CalcFrame() {
+		
+		initUi();
+		setVisible(true);
+	}
+
+	private void initUi() {
+		
+		setTitle("计算器-qinyq作品");
+		setSize(320,480);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		//添加结果标签
+		
+		labelResult=new JLabel("0.0");
+		labelResult.setBorder(new EmptyBorder(20,10,20,10));
+		labelResult.setHorizontalAlignment(SwingConstants.RIGHT);
+		labelResult.setFont(new Font("微软雅黑",Font.PLAIN,32));
+		add(labelResult,BorderLayout.NORTH);
+		
+		
+		//设置布局
+		JPanel buttonPanel=new JPanel(new GridLayout(5,4,5,5));
+		//按钮面板添加到窗口的中间
+		add(buttonPanel,BorderLayout.CENTER);
+		
+		for(int i=0;i<titles.length;i++){
+			if(titles[i].length()==0){
+				//不显示内容的标签
+				buttonPanel.add(new JLabel(""));
+			}else{
+				//按钮
+			 buttons[i] =new JButton(titles[i]);
+				buttonPanel.add(buttons[i]);
+			}
+		}
+	}
 
 	
 }
